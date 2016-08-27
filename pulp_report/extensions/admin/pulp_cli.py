916832cb3d7ac73b94907da26deaafc4df047835
ulif <uli@gnufix.de>
2016-08-28 00:46:16 +0200

    Use more elaborate setup, avoiding globals.

--- pulp_report/extensions/admin/pulp_cli.py
+++ pulp_report/extensions/admin/pulp_cli.py
@@ -1,22 +1,51 @@
 import datetime
+from gettext import gettext as _
 from pulp.client.extensions.decorator import priority
+from pulp.client.extensions.extensions import PulpCliSection, PulpCliCommand
 
-CONTEXT = None
+
+PROFILEDIFF_DESC = _("Show profile differences.")
+SECT_DESC = _('create reports')
 
 @priority()
 def initialize(context):
     """
     :type context: pulp.client.extensions.core.ClientContext
     """
-    global CONTEXT
-    CONTEXT = context
+    report_section = ReportSection(context)
+    context.cli.add_section(report_section)
 
-    report_section = context.cli.create_section('report', 'Report section')
-    profilediff_command = report_section.create_command('profilediff', 'Generate reports', run_profilediff)
-    profilediff_command.create_option('--name', 'Name of the user', required=True)
-    profilediff_command.create_flag('--show-date', 'If specified, the date will be displayed')
 
-def run_profilediff(**kwargs):
-    CONTEXT.prompt.write('Hello %s' % kwargs['name'])
-    if kwargs['show-date']:
-        CONTEXT.prompt.write(datetime.datetime.now())
+class ReportSection(PulpCliSection):
+
+    def __init__(self, context):
+        """
+        @param context:
+        @type  context: pulp.client.extensions.core.ClientContext
+        """
+        PulpCliSection.__init__(self, 'report', SECT_DESC)
+        self.context = context
+        self.prompt = context.prompt  # for easier access
+
+        self.add_command(ProfilediffReport(context))
+
+
+class ProfilediffReport(PulpCliCommand):
+
+    def __init__(self, context, name='profilediff', description=PROFILEDIFF_DESC, method=None):
+        self.context = context
+        self.prompt = context.prompt
+
+        if method is None:
+            method = self.run
+
+        super(ProfilediffReport, self).__init__(name, description, method)
+
+        self.create_option('--name', 'Name of the user', required=True)
+        self.create_flag('--show-date', 'If specified, the date will be displayed')
+
+
+    def run(self, **kwargs):
+        self.prompt.write('Hello %s' % kwargs['name'])
+        if kwargs['show-date']:
+            self.prompt.write(datetime.datetime.now())
