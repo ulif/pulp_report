665fadc1e364c31280da554e2db9765e7216c2de
ulif <uli@gnufix.de>
2016-08-30 14:21:57 +0200

    Provide proper long description.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='pulp_report',
     version='0.1.dev0',
     license='GPLv2+',
+    long_description=read('README.rst') + '\n\n\n' + read('CHANGES.rst'),
     packages=find_packages(exclude=['test', 'test.*']),
     author='Uli Fouquet',
     author_email='uli@gnufix.de',
